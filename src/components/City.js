78fe507a399b120101bc6db2ad3991998fded0dc
Vincent Costa <costa.vincent132@gmail.com>
2020-03-21 16:23:34 -0700

    removed div in city

--- src/components/City.js
+++ src/components/City.js
@@ -1,17 +1,12 @@
 import React from "react"
-import { Card, CardActions, CardHeader } from "@material-ui/core"
+import { Card, CardHeader } from "@material-ui/core"
 
 export default props => {
     const classStr = props.active ? "city active" : "city"
 
     return (
-        <div className={classStr} onClick={props.onClick}>
-            <Card>
-                <CardActions>
-
-                </CardActions>
-                <CardHeader title={props.city.name || "Fallbrook"} />
-            </Card>
-        </div>
+        <Card className = {classStr} onClick = {props.onClick}>
+            <CardHeader title={props.city.name || "Fallbrook"} />
+        </Card>
     )
 }
